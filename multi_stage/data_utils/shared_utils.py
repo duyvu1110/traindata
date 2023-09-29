15419259ae948fac17cf8c9ee29d154028a2e4ac
UwU-tao <hieungo1410@gmail.com>
2023-09-29 08:36:21 +0700

    tt

--- multi_stage/data_utils/shared_utils.py
+++ multi_stage/data_utils/shared_utils.py
@@ -285,23 +285,24 @@ def get_token_col(sent_col, split_symbol=None, bert_tokenizer=None, dim=1, add_n
         else:
             if add_next_sent is None:
                 # return bert_tokenizer.tokenize('[CLS] ' + sent_col + ' [SEP]')
-                return bert_tokenizer.tokenize(sent_col)
+                temp = bert_tokenizer(sent_col)
+                temp['input_ids'][0] = 101
+                temp['input_ids'][-1] = 102
+                temp['input_ids'].insert(0, 101)
+                temp['input_ids'].append(102)
+                temp['token_type_ids'].append(0)
+                temp['token_type_ids'].append(0)
+                temp['attention_mask'].append(1)
+                temp['attention_mask'].append(1)
+                # return bert_tokenizer.tokenize(sent_col)
+                return temp
             else:
                 return bert_tokenizer.tokenize('[CLS] ' + sent_col + ' [SEP] ' + add_next_sent + ' [SEP]')
     else:
         token_col = []
 
         for index in range(len(sent_col)):
-            temp = get_token_col(sent_col[index], split_symbol, bert_tokenizer, dim - 1)
-            temp['input_ids'][0] = 101
-            temp['input_ids'][-1] = 102
-            temp['input_ids'].insert(0, 101)
-            temp['input_ids'].append(102)
-            temp['token_type_ids'].append(0)
-            temp['token_type_ids'].append(0)
-            temp['attention_mask'].append(1)
-            temp['attention_mask'].append(1)
-            # token_col.append(get_token_col(sent_col[index], split_symbol, bert_tokenizer, dim - 1))
+            token_col.append(get_token_col(sent_col[index], split_symbol, bert_tokenizer, dim - 1))
             token_col.append(temp)
 
         return token_col
