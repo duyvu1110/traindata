2a6d1cb33147024c26b7b07e94356a6f9e0f99d8
UwU-tao <hieungo1410@gmail.com>
2023-10-04 16:22:00 +0700

    tt

--- multi_stage/data_utils/shared_utils.py
+++ multi_stage/data_utils/shared_utils.py
@@ -495,10 +495,8 @@ def token_mapping_bert(bert_token_col, gold_token_col):
                 print('len_gold', len(seq_gold_token[token_index]))
                 bert_length += len(seq_bert_token[bert_index]) + 1
                 print(bert_length, token_length)
-                # if seq_bert_token[bert_index].find("@@") != -1:
-                #     bert_length -= 2
-                # else:
-                #     bert_length += len(seq_bert_token[bert_index])
+                if seq_bert_token[bert_index].find("@@") != -1:
+                    bert_length -= 2
 
             # print (bert_length, '\t', token_length)
             assert bert_length == token_length, "appear mapping error!"
