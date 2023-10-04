8bda800a88ad0d111353dbad4764803c2085c1e5
UwU-tao <hieungo1410@gmail.com>
2023-10-04 23:33:05 +0700

    tt

--- multi_stage/data_utils/current_program_code.py
+++ multi_stage/data_utils/current_program_code.py
@@ -161,8 +161,10 @@ def convert_eng_label_dict_by_mapping(label_col, mapping_col):
                 s_index = elem_position[0]
                 e_index = elem_position[1]
                 
-                s_index = list(sequence_map.keys())[list(sequence_map.values()).index(s_index)]
-                e_index = list(sequence_map.keys())[list(sequence_map.values()).index(e_index)]
+                # s_index = list(sequence_map.keys())[list(sequence_map.values()).index(s_index)]
+                # e_index = list(sequence_map.keys())[list(sequence_map.values()).index(e_index)]
+                s_index = next((key for key, value_list in sequence_map.items() if s_index in value_list), -1)
+                e_index = next((key for key, value_list in sequence_map.items() if e_index in value_list), -1)
                 
                 print(s_index, e_index)
                 print(sequence_label[key][elem_index])
