03784ef6ed624ed7d4ee614c1207429775354f47
UwU-tao <hieungo1410@gmail.com>
2023-10-04 23:04:26 +0700

    tt

--- multi_stage/data_utils/current_program_code.py
+++ multi_stage/data_utils/current_program_code.py
@@ -169,7 +169,12 @@ def convert_eng_label_dict_by_mapping(label_col, mapping_col):
                 if s_index == -1 or e_index == -1:
                     sequence_label[key][elem_index] = [-1, -1]
                 else:
-                    sequence_label[key][elem_index] = [sequence_map[s_index][0], sequence_map[e_index][-1]]
+                    # sequence_label[key][elem_index] = [sequence_map[s_index][0], sequence_map[e_index][-1]]
+                    try:
+                        sequence_label[key][elem_index] = [sequence_map[s_index][0], sequence_map[e_index][-1]]
+                    except:
+                        print(sequence_label[key])
+                        return
 
                 if key == "result":
                     sequence_label[key][elem_index].append(elem_position[-1])
