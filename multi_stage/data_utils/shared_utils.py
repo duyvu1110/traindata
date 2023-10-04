6c81915d43f87a625c4a8fccc48497a6b1c31ea6
UwU-tao <hieungo1410@gmail.com>
2023-10-04 17:36:17 +0700

    tt

--- multi_stage/data_utils/shared_utils.py
+++ multi_stage/data_utils/shared_utils.py
@@ -496,7 +496,7 @@ def token_mapping_bert(bert_token_col, gold_token_col):
                 # bert_length += len(seq_bert_token[bert_index]) + 1
                 
                 if seq_bert_token[bert_index-1].find("@@") != -1:
-                    bert_length = bert_length + len(seq_bert_token[bert_index]) - 2
+                    bert_length = bert_length + len(seq_bert_token[bert_index])
                     if seq_bert_token[bert_index].find("@@") != -1:
                          bert_length = bert_length - 2
                 else:
