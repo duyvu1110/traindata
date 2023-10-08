d7612674011788e38f8e6bbcc971bca989bfae78
UwU-tao <hieungo1410@gmail.com>
2023-10-09 00:05:03 +0700

    test

--- multi_stage/data_utils/current_program_code.py
+++ multi_stage/data_utils/current_program_code.py
@@ -630,8 +630,7 @@ def create_polarity_train_data(config, tuple_pair_col, feature_out, bert_feature
                 cur_representation = torch.cat(each_pair_representation, dim=-1).view(-1).numpy().tolist()
 
             representation_col.append(cur_representation)
-            print(representation_col)
-            print(polarity_col)
+
             assert tuple_pair_col[index][pair_index][-1][0] in {-1, 0, 1, 2, 3, 4, 5, 6}, "[ERROR] Tuple Pair Col Error."
             polarity_col.append([tuple_pair_col[index][pair_index][-1][0] + 1])
 
