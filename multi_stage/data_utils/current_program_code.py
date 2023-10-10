9d3ceab898804e93bcf3504c62a6d850ca7a76b2
UwU-tao <hieungo1410@gmail.com>
2023-10-11 01:36:17 +0700

    t

--- multi_stage/data_utils/current_program_code.py
+++ multi_stage/data_utils/current_program_code.py
@@ -157,8 +157,8 @@ def convert_vi_label_dict_by_mapping(label_col, mapping_col):
 
         for key, elem_position_col in sequence_label.items():
             for elem_index, elem_position in enumerate(elem_position_col):
-                s_index = elem_position[0] -1
-                e_index = elem_position[1] -1
+                s_index = elem_position[0]
+                e_index = elem_position[1]
                 
                 # s_index = next((key for key, value_list in sequence_map.items() if s_index in value_list), -1)
                 # e_index = next((key for key, value_list in sequence_map.items() if e_index in value_list), -1)
@@ -167,7 +167,7 @@ def convert_vi_label_dict_by_mapping(label_col, mapping_col):
                 if s_index == -1 or e_index == -1:
                     sequence_label[key][elem_index] = [-1, -1]
                 else:
-                    sequence_label[key][elem_index] = [sequence_map[s_index][0], sequence_map[e_index][-1]]
+                    sequence_label[key][elem_index] = [sequence_map[s_index], sequence_map[e_index]]
 
                 if key == "result":
                     sequence_label[key][elem_index].append(elem_position[-1])
@@ -754,7 +754,7 @@ def convert_vi_tuple_pair_by_mapping(tuple_pair_col, mapping_col):
                     new_tuple_pair.append((-1, -1))
                     continue
 
-                new_s_index, new_e_index = sequence_map[s_index - 1][0], sequence_map[e_index - 1][-1]
+                new_s_index, new_e_index = sequence_map[s_index], sequence_map[e_index]
                 new_tuple_pair.append((new_s_index, new_e_index))
 
             # add polarity.
