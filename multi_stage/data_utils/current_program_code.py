c65eb816234510d2ca4bc2e3e603ede6a32c7efb
UwU-tao <hieungo1410@gmail.com>
2023-10-10 23:40:55 +0700

    t

--- multi_stage/data_utils/current_program_code.py
+++ multi_stage/data_utils/current_program_code.py
@@ -164,14 +164,7 @@ def convert_vi_label_dict_by_mapping(label_col, mapping_col):
                 if s_index == -1 or e_index == -1:
                     sequence_label[key][elem_index] = [-1, -1]
                 else:
-                    try:
-                        sequence_label[key][elem_index] = [sequence_map[s_index][0], sequence_map[e_index][-1]]
-                    except:
-                        print(s_index)
-                        print(e_index)
-                        print(sequence_label)
-                        print(sequence_map)
-                        # print(key)
+                    sequence_label[key][elem_index] = [sequence_map[s_index][0], sequence_map[e_index][-1]]
 
                 if key == "result":
                     sequence_label[key][elem_index].append(elem_position[-1])
