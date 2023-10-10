6d0158dd4bf8a0ccc0fc5e2381f5164c82fb37d3
UwU-tao <hieungo1410@gmail.com>
2023-10-10 23:56:18 +0700

    t

--- multi_stage/data_utils/current_program_code.py
+++ multi_stage/data_utils/current_program_code.py
@@ -160,8 +160,9 @@ def convert_vi_label_dict_by_mapping(label_col, mapping_col):
                 s_index = elem_position[0] + 1
                 e_index = elem_position[1]
                 
-                s_index = list(sequence_map.keys())[list(sequence_map.values()).index(s_index)]
-                e_index = list(sequence_map.keys())[list(sequence_map.values()).index(e_index)]
+                s_index = next((key for key, value_list in sequence_map.items() if s_index in value_list), -1)
+                e_index = next((key for key, value_list in sequence_map.items() if e_index in value_list), -1)
+                
                 # 针对英文数据集可能存在空的情况
                 if s_index == -1 or e_index == -1:
                     sequence_label[key][elem_index] = [-1, -1]
