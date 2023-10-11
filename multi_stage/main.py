b75ef846eda7b5cdd0c94d260cbe160173d8b739
UwU-tao <hieungo1410@gmail.com>
2023-10-11 10:04:49 +0700

    t

--- multi_stage/main.py
+++ multi_stage/main.py
@@ -397,31 +397,31 @@ def main():
         dev_first_process_data_path = "./ModelResult/" + model_name + "/dev_first_data_" + str(feature_type) + ".txt"
         test_first_process_data_path = "./ModelResult/" + model_name + "/test_first_data_" + str(feature_type) + ".txt"
 
-        if os.path.exists(train_first_process_data_path):
-            train_pair_representation, train_make_pair_label, train_polarity_representation, train_polarity_label = \
-                shared_utils.read_pickle(train_first_process_data_path)
-        else:
-            _, train_pair_representation, train_make_pair_label, train_feature_out, train_bert_feature_out = \
-                train_test_utils.first_stage_model_test(
-                    elem_model, config, train_loader, generate_second_res_eval,
-                    eval_parameters=[data_gene.train_data_dict['tuple_pair_col']],
-                    test_type="gene", feature_type=feature_type
-                )
-
-            train_pair_representation, train_make_pair_label = cpc.generate_train_pair_data(
-                train_pair_representation, train_make_pair_label
+    # if os.path.exists(train_first_process_data_path):
+    #     train_pair_representation, train_make_pair_label, train_polarity_representation, train_polarity_label = \
+    #         shared_utils.read_pickle(train_first_process_data_path)
+    # else:
+        _, train_pair_representation, train_make_pair_label, train_feature_out, train_bert_feature_out = \
+            train_test_utils.first_stage_model_test(
+                elem_model, config, train_loader, generate_second_res_eval,
+                eval_parameters=[data_gene.train_data_dict['tuple_pair_col']],
+                test_type="gene", feature_type=feature_type
             )
 
-            train_polarity_representation, train_polarity_label = cpc.create_polarity_train_data(
-                config, data_gene.train_data_dict['tuple_pair_col'], train_feature_out,
-                train_bert_feature_out, feature_type=feature_type
-            )
+        train_pair_representation, train_make_pair_label = cpc.generate_train_pair_data(
+            train_pair_representation, train_make_pair_label
+        )
 
-            shared_utils.write_pickle(
-                [train_pair_representation, train_make_pair_label,
-                 train_polarity_representation, train_polarity_label],
-                train_first_process_data_path
-            )
+        train_polarity_representation, train_polarity_label = cpc.create_polarity_train_data(
+            config, data_gene.train_data_dict['tuple_pair_col'], train_feature_out,
+            train_bert_feature_out, feature_type=feature_type
+        )
+
+        shared_utils.write_pickle(
+            [train_pair_representation, train_make_pair_label,
+                train_polarity_representation, train_polarity_label],
+            train_first_process_data_path
+        )
 
         if os.path.exists(dev_first_process_data_path):
             dev_candidate_pair_col, dev_pair_representation, dev_make_pair_label = \
