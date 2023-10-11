d94ecc57596eacd25695036d991c6684eb9a2f27
UwU-tao <hieungo1410@gmail.com>
2023-10-11 08:28:33 +0700

    t

--- multi_stage/data_utils/current_program_code.py
+++ multi_stage/data_utils/current_program_code.py
@@ -147,7 +147,7 @@ def convert_vi_label_dict_by_mapping(label_col, mapping_col):
     for index in range(len(label_col)):
         sequence_label, sequence_map = copy.deepcopy(label_col[index]), mapping_col[index]
 
-        # print(label_col[index], '\n', mapping_col[index])
+        print(label_col[index], '\n', mapping_col[index])
         
         for key in sequence_label.keys():
             sequence_label[key] = sorted(list(sequence_label[key]), key=lambda x: x[0])
