a3c54bf9c5988017ab4bc610e9b47b58f18347a0
UwU-tao <hieungo1410@gmail.com>
2023-10-21 19:48:32 +0700

    test

--- multi_stage/run.sh
+++ multi_stage/run.sh
@@ -1,3 +1,3 @@
-python main.py --file_type="smartphone" --model_mode="bert" --program_mode="run" --stage_model="first" --epoch=20 --model_type="multitask" --embed_dropout=0.1
+python main.py --file_type="smartphone" --model_mode="bert" --program_mode="run" --stage_model="first" --epoch=2 --model_type="multitask" --embed_dropout=0.1
 
-python main.py --file_type="smartphone" --model_mode="bert" --program_mode="run" --stage_model="second" --epoch=20 --model_type="multitask" --embed_dropout=0.1 --factor=0.3
+python main.py --file_type="smartphone" --model_mode="bert" --program_mode="run" --stage_model="second" --epoch=2 --model_type="multitask" --embed_dropout=0.1 --factor=0.3
