12b7cd8c52e16e1433adba7a219b2b1c718c5da0
UwU-tao <hieungo1410@gmail.com>
2023-10-22 20:05:05 +0700

    checkpoint

--- multi_stage/eval_utils/base_eval.py
+++ multi_stage/eval_utils/base_eval.py
@@ -1311,8 +1311,8 @@ class PairEvaluation(BaseEvaluation):
                     write_str += "], "
                     
             else:
-                # print(tuple_pair[index][0] + 1)
-                write_str += polarity_col[predict_tuple_pair[index][0] + 1] + "}\n"
+                print(tuple_pair[index][0] + 1)
+                write_str += polarity_col[int(predict_tuple_pair[index][0]) + 1] + "}\n"
                 
             # write_str += self.tuple_pair_to_string(predict_tuple_pair[index], sentence)
 
